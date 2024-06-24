0e3ea72822737f2eb58b705a7330c6ce65909c85
APOgamer <adrianpalmaobi23@gmail.com>
2024-06-24 17:00:54 -0500

    Update Flight.java

--- TripMateBackend/src/main/java/com/backend/tripmate/flights/domain/model/entities/Flight.java
+++ TripMateBackend/src/main/java/com/backend/tripmate/flights/domain/model/entities/Flight.java
@@ -6,15 +6,21 @@ import java.time.LocalDateTime;
 
 @Entity
 public class Flight {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idVuelo;
+
     private String nombreAerolinea;
     private String imagePath;
+
     @Column(name = "descripcion", nullable = false, length = 1500)
     private String descripcion;
+
     private String precio;
 
+    public Flight() {
+    }
 
     // Getters and Setters
     public int getIdVuelo() {
@@ -29,16 +35,16 @@ public class Flight {
         return nombreAerolinea;
     }
 
+    public void setNombreAerolinea(String nombreAerolinea) {
+        this.nombreAerolinea = nombreAerolinea;
+    }
+
     public String getImagePath() {
         return imagePath;
     }
 
     public void setImagePath(String imagePath) {
         this.imagePath = imagePath;
-    }
-
-    public void setNombreAerolinea(String nombreAerolinea) {
-        this.nombreAerolinea = nombreAerolinea;
     }
 
     public String getDescripcion() {
@@ -57,4 +63,10 @@ public class Flight {
         this.precio = precio;
     }
 
+    public Flight(String nombreAerolinea, String imagePath, String descripcion, String precio) {
+        this.nombreAerolinea = nombreAerolinea;
+        this.imagePath = imagePath;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
 }
